d9348f34e52177626708fcd69491594a9bd336cf
Saam0 <Samvelyazhyan@gmail.com>
2026-06-25 11:39:03 +0400

    feat: տեղափոխվել է LONG և SHORT մուտքի, ATR-ի ու դինամիկ SL-ի տրամաբանությունը ստրատեգիայի հիմնական ֆայլ

--- user_data/strategies/MySupertrand_V5.py
+++ user_data/strategies/MySupertrand_V5.py
@@ -36,6 +36,37 @@ class MySupertrand_V5(IStrategy):
         dataframe["st_line"] = st[st.columns[0]]
         dataframe["st_dir"] = st[st.columns[1]]
 
+        # --- ATR և Նախորդ Մոմերի Պատմություն ---
+        dataframe['atr'] = pta.atr(dataframe['high'], dataframe['low'], dataframe['close'], length=14)
+        dataframe['prev_high'] = dataframe['high'].shift(1)
+        dataframe['prev_low'] = dataframe['low'].shift(1)
+
+        # --- Դինամիկ Stop Loss-ի Գծեր ---
+        dataframe['dynamic_sl_long'] = dataframe['prev_low'] - dataframe['atr']
+        dataframe['dynamic_sl_short'] = dataframe['prev_high'] + dataframe['atr']
+
+        # --- LONG Տրամաբանություն ---
+        dataframe['trend_changed_to_long'] = np.where((dataframe['st_dir'] == 1) & (dataframe['st_dir'].shift(1) == -1),
+                                                      True, False)
+        dataframe['entry_price_anchor'] = np.nan
+        dataframe.loc[dataframe['trend_changed_to_long'] == True, 'entry_price_anchor'] = dataframe['close']
+        dataframe['entry_price_anchor'] = dataframe['entry_price_anchor'].ffill()
+
+        dataframe['enter_long'] = 0
+        dataframe.loc[
+            (dataframe['st_dir'] == 1) & (dataframe['close'] <= dataframe['entry_price_anchor']), 'enter_long'] = 1
+
+        # --- SHORT Տրամաբանություն ---
+        dataframe['trend_changed_to_short'] = np.where(
+            (dataframe['st_dir'] == -1) & (dataframe['st_dir'].shift(1) == 1), True, False)
+        dataframe['short_entry_anchor'] = np.nan
+        dataframe.loc[dataframe['trend_changed_to_short'] == True, 'short_entry_anchor'] = dataframe['close']
+        dataframe['short_entry_anchor'] = dataframe['short_entry_anchor'].ffill()
+
+        dataframe['enter_short'] = 0
+        dataframe.loc[
+            (dataframe['st_dir'] == -1) & (dataframe['close'] >= dataframe['short_entry_anchor']), 'enter_short'] = 1
+
 
         return dataframe
 
